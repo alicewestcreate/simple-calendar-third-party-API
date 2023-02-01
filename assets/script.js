c7b73e4721a01453fa17033a7c8009f0f64b5d06
alice west <alicewest.create@gmail.com>
2023-02-01 11:48:18 +0000

    update

--- assets/script.js
+++ assets/script.js
@@ -45,6 +45,8 @@ let createSubjectCell = function () {
     let cell = $("<textarea>")
     cell.addClass("col-9")
     cell.attr("data-cell", "subject-cell")
+    let storedInfo = localStorage.getItem(`${HH}:00`)
+    cell.text(storedInfo) || ("")
     return(cell)
 }
 let createStatusCell = function () {
@@ -66,8 +68,8 @@ let createEntryRow = function () {
 
 let appear = function () {
 
-    containerEl.html("")
-    let HH = 08
+    // containerEl.html("")
+    HH = 08
 
     for (let i = 0; i < 9; i++ ) {
         timeCell = createTimeCell()
@@ -78,6 +80,9 @@ let appear = function () {
         entryRow.append(timeCell, subjectCell, statusCell)
         containerEl.append(entryRow)
     }
+
+
+
     
 }
 
@@ -99,33 +104,49 @@ appear()
 // ------------------------------
 
 
-let currentHour = today.format("HH")
-
-// let currentHour = 12
+let currentHour = moment().format("HH")
 let allrows = containerEl.children()
+let freshTimeblocks = function() {
 
-// THIS IS NEED TO BE WRAPPED IN A SETINTERVAL REFRESH TIMER! 
-
-for (let index = 0; index < allrows.length; index++) {
-    let row = allrows.eq([index])
-    let dataRow = row.attr("data-row")
-    let subjectCol = row.children().eq([1])
-
-    if (dataRow  === currentHour) {
-        subjectCol.addClass("present")
-        subjectCell.removeClass("future past")
-
-    } else if (dataRow > currentHour) {
-        subjectCol.addClass("future")
-        subjectCell.removeClass("present past")
-
-    } else if (dataRow < currentHour) {
-        subjectCol.addClass("past")
-        subjectCell.removeClass("future")
-    }
-    
     // QUESTION - HOW TO STOP COMPUTER THINKING 9 IS GREAT THAN 12 BECUASE IT STARTS WITH A ONE. 
+
+
+    for (let index = 0; index < allrows.length; index++) {
+        let row = allrows.eq([index])
+        let dataRow = row.attr("data-row")
+        let subjectCol = row.children().eq([1])
+    
+        if (dataRow  === currentHour) {
+            console.log(dataRow)
+            console.log(" IS Equal TO ")
+            console.log(currentHour)
+            subjectCol.addClass("present")
+            subjectCell.removeClass("future past")
+    
+        } else if (dataRow > currentHour) {
+            
+            console.log(dataRow)
+            console.log("IS greater THAN")
+            console.log(currentHour)
+            subjectCol.addClass("future")
+            subjectCell.removeClass("present past")
+    
+        } else if (dataRow < currentHour) {
+            console.log(dataRow)
+            console.log("IS less than")
+            console.log(currentHour)
+            subjectCol.addClass("past")
+            subjectCell.removeClass("future")
+        }
+    }
 }
+
+// setInterval(freshTimeblocks, 1000);
+freshTimeblocks()
+
+
+
+
 
 
 // ------------------------------
@@ -135,137 +156,25 @@ for (let index = 0; index < allrows.length; index++) {
 // - This already happens when the element is dynamically created. 
 
 
-
-
-
 // ------------------------------
 //  TASK FIVE - Save the event in local storage when the save button is clicked in that timeblock
 // ------------------------------
 
 
+let saveEntry = function() {
 
-// Introducing new element for the button. 
-let saveButton = $("button")
-// console.log(saveButton)
+    for (let index = 0; index < allrows.length; index++) {
+        let firstRow = allrows.eq([index])
+        let time = firstRow.children().eq([0]).text()
+        let entry = firstRow.children().eq([1]).val()
+        console.log(time);
+        console.log(entry);
+        localStorage.setItem(time, entry)
+    }
+}
 
+allrows.on("click", saveEntry)
 
-
-containerEl.on("click","button", function(event){
-    let sib = $(event.target)
-
-    let sibling = $(event.target.previousElementSiblings)
-
-
-    console.log(event)
-    console.log(sib)
-    console.log(sibling)
-
-    console.log("click")
-
-
-}) 
-
-
-
-// console.log("entry save")
-// let firstRow = allrows.eq([0])
-// let time = firstRow.children().eq([0]).text()
-// let entry = firstRow.children().eq([1]).val()
-// localStorage.setItem(time, entry)
-
-
-
-
-
-// // for (let index = 0; index < allrows.length; index++) {
-
-
-    
-// //     const element = array[index];
-    
-// // }
-
-
-
-
-
-
-
-// // saveButton.forEach(button => {
-// //     let siblings = button.siblings()
-// //    console.log(siblings)
-    
-// // });
-
-
-// // let saveEntry = function() {
-// //     console.log(saveButton);
-// //     for (let index = 0; index < allrows.length; index++) {
-// //         let row = allrows.eq([index])
-
-// //         let time = row.children().eq([0]).text()
-// //         let entry = row.children().eq([1]).val()
-// //         let saveItem = row.children().eq([2])
-// //         localStorage.setItem(time, entry)
-
-// //     }
-
-// // }
-
-
-// // saveButton.on("click", saveEntry)
-
-
-
-
-
-
-
-// let subject = allrows.eq([0]).children().eq([1])
-// let saveButton = allrows.eq([0]).children().eq([2])
-
-// subject.text("First item")
-
-
-// let enterEntry = function() {
-//     console.log(" Enter Entry") 
-// }
-
-
-// select all rows, and iterate over each row, and make this function happen
-
-
-
-    // for (let index = 0; index < allrows.length; index++) {
-    //     let subject = allrows.eq([index]).children().eq([1])
-    //     let saveButton = allrows.eq([index]).children().eq([2])
-    //     // console.log("log", index, allrows.eq([index]));
-    // }
-
-
-    // console.log("entry save")
-    // let firstRow = allrows.eq([0])
-    // let time = firstRow.children().eq([0]).text()
-    // let entry = firstRow.children().eq([1]).val()
-    // localStorage.setItem(time, entry)
-
-
-    // for (let index = 0; index < allrows.length; index++) {
-    //     console.log("entry save")
-    //     let row = allrows.eq([index])
-    //     let time = row.children().eq([0]).text()
-    //     let entry = row.children().eq([1]).val()
-    //     localStorage.setItem(time, entry) 
-    //        }
-
-
-
-
-
-
-
-
-// saveButton.on("click", saveEntry)
 
 
 
@@ -275,6 +184,25 @@ containerEl.on("click","button", function(event){
 //  TASK SIX - Persist events between refreshes of a page
 // ------------------------------
 
+// if today is greater than yesterday or 1, then clear storage 
+
+
+
+let previousSession = today.format("DD")
+localStorage.setItem("Previous Session", previousSession)
+
+
+let currentSession = today.format("DD")+1
+
+// if today is great then previous session, clear session.
+
+
+
+
+console.log(previousSession);
+
+console.log(currentSession);
+localStorage.setItem("Current Session", currentSession)
 
 
 
